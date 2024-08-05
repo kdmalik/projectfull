0a5fa5b878d5432ad0ad4b43c657f0efbd624ab8
kdmalik <161879632+kdmalik@users.noreply.github.com>
2024-08-05 23:35:36 +0530

    ddddd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,57 +2,110 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import { Footer } from "./Footer";
 import { Container, Table, Button, Form, Pagination } from "react-bootstrap";
-import axiosInstance from '../api/axiosInstance.js';
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import * as XLSX from "xlsx";
 
 export const Home = () => {
   const [users, setUsers] = useState([]);
-  const [customData, setCustomData] = useState([]);
-  const [newData, setNewData] = useState("");
   const [editId, setEditId] = useState(null);
-  const [editData, setEditData] = useState("");
+  const [editName, setEditName] = useState("");
+  const [editEmail, setEditEmail] = useState("");
+  const [editPhone, setEditPhone] = useState("");
+  const [searchEmail, setSearchEmail] = useState("");
+  const [searchName,setSearchName]=useState("");
+  const [searchPhone,setSearchPhone]=useState("");
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 10;
 
   useEffect(() => {
-    fetchCustomData();
+    const storedUsers = JSON.parse(localStorage.getItem("register")) || [];
+    setUsers(storedUsers);
   }, []);
 
-  const fetchCustomData = async () => {
-    try {
-      const response = await axiosInstance.get(`/customData/${'66af5bf69f942c04643fec54'}`);
-      setCustomData(response.data);
-    } catch (error) {
-      console.error("Error fetching custom data", error);
+  const handleDelete = (id) => {
+    const newUsers = users.filter((user) => user.id !== id);
+    setUsers(newUsers);
+    localStorage.setItem("register", JSON.stringify(newUsers));
+  };
+
+  const handleDeleteSelected = () => {
+    const newUsers = users.filter((user) => !selectedUsers.includes(user.id));
+    setUsers(newUsers);
+    setSelectedUsers([]);
+    localStorage.setItem("register", JSON.stringify(newUsers));
+  };
+
+  const handleEdit = (user) => {
+    setEditId(user.id);
+    setEditName(user.name);
+    setEditEmail(user.email);
+    setEditPhone(user.phone);
+  };
+
+  const handleSave = (id) => {
+    const updatedUsers = users.map((user) =>
+      user.id === id
+        ? { ...user, name: editName, email: editEmail, phone: editPhone }
+        : user
+    );
+    setUsers(updatedUsers);
+    localStorage.setItem("register", JSON.stringify(updatedUsers));
+    setEditId(null);
+  };
+
+  const handleDownloadPDF = () => {
+    const doc = new jsPDF();
+    autoTable(doc, {
+      head: [['#', 'UID', 'Name', 'Email', 'Phone']],
+      body: users.map((user, index) => [
+        index + 1,
+        user.id,
+        user.name,
+        user.email,
+        user.phone,
+      ]),
+    });
+    doc.save('users.pdf');
+  };
+
+  const handleDownloadXLSX = () => {
+    const worksheet = XLSX.utils.json_to_sheet(users);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Users');
+    XLSX.writeFile(workbook, 'users.xlsx');
+  };
+
+  const handleSelectUser = (id) => {
+    setSelectedUsers((prevSelected) =>
+      prevSelected.includes(id)
+        ? prevSelected.filter((userId) => userId !== id)
+        : [...prevSelected, id]
+    );
+  };
+
+  const handleSelectAll = () => {
+    if (selectedUsers.length === users.length) {
+      setSelectedUsers([]);
+    } else {
+      setSelectedUsers(users.map((user) => user.id));
     }
   };
 
-  const handleAddData = async () => {
-    try {
-      const response = await axiosInstance.post('/customData/add', { userId:"66af5bf69f942c04643fec54", data: newData });
-      setCustomData([...customData, response.data.customData]);
-      setNewData("");
-    } catch (error) {
-      console.error("Error adding custom data", error);
-    }
-  };
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(searchName.toLowerCase()) &&
+      user.email.toLowerCase().includes(searchEmail.toLowerCase())&&
+      user.phone.toLowerCase().includes(searchPhone.toLowerCase())
+  );
 
-  const handleEditData = async (id) => {
-    try {
-      const response = await axiosInstance.put(`/customData/${id}`, { data: editData });
-      setCustomData(customData.map(data => data._id === id ? response.data.updatedData : data));
-      setEditId(null);
-      setEditData("");
-    } catch (error) {
-      console.error("Error editing custom data", error);
-    }
-  };
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+  const totalPages = Math.ceil(filteredUsers.length / usersPerPage);
 
-  const handleDeleteData = async (id) => {
-    try {
-      await axiosInstance.delete(`/customData/${id}`);
-      setCustomData(customData.filter(data => data._id !== id));
-    } catch (error) {
-      console.error("Error deleting custom data", error);
-    }
-  };
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="d-flex flex-column min-vh-100">
@@ -61,46 +114,112 @@ export const Home = () => {
         <h1 style={{ textAlign: "center", padding: "20px" }}>
           Welcome to My WebPage
         </h1>
-        <Form.Group controlId="newData" className="mb-3" style={{ display: "flex", justifyContent: 'flex-start', gap: '10px', margin: '5px' }}>
+        <Form.Group controlId="search" className="mb-3" style={{ display: "flex", justifyContent: 'flex-start', gap: '10px', margin: '5px' }}>
+          
           <Form.Control
             type="text"
-            placeholder="Enter new data"
-            value={newData}
-            onChange={(e) => setNewData(e.target.value)}
-            style={{ width: '70%', backgroundColor: '#f8f9fa', color: '#343a40', border: '1px solid #adb5bd' }}
+            placeholder="Search by name or email"
+            value={searchEmail}
+            onChange={(e) => setSearchEmail(e.target.value)}
+            style={{ width: '30%', backgroundColor: '#f8f9fa', color: '#343a40', border: '1px solid #adb5bd' }}
           />
-          <Button variant="success" onClick={handleAddData}>Add Data</Button>
+          <Form.Control
+            type="text"
+            placeholder="Search by name "
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+            style={{ width: '30%', backgroundColor: '#f8f9fa', color: '#343a40', border: '1px solid #adb5bd' }}
+          />
+          <Form.Control
+            type="text"
+            placeholder="Search by phone "
+            value={searchPhone}
+            onChange={(e) => setSearchPhone(e.target.value)}
+            style={{ width: '30%', backgroundColor: '#f8f9fa', color: '#343a40', border: '1px solid #adb5bd' }}
+          />
         </Form.Group>
+        <div/>
+        <div style={{ display: "flex", justifyContent: 'flex-start', gap: '10px', margin: '5px' }}>
+          <Button variant="danger" onClick={handleDeleteSelected} className="justify-content-end">
+            Delete 
+          </Button>
+          <Button variant="success" onClick={handleDownloadPDF}>Download PDF</Button>
+          <Button variant="success" onClick={handleDownloadXLSX}>Download XLSX</Button>
+        </div>
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>#</th>
-              <th>Custom Data</th>
-              <th>Actions</th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>
+                 <div>Select All</div>
+                <Form.Check
+                  type="checkbox" 
+                  checked={selectedUsers.length === users.length}
+                  onChange={handleSelectAll}
+                />
+                
+              </th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>#</th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>UID</th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>Name</th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>Email</th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>Phone</th>
+              <th style={{ backgroundColor: "lightgoldenrodyellow" }}>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {customData.map((data, index) => (
-              <tr key={data._id}>
-                <td>{index + 1}</td>
+            {currentUsers.map((user, index) => (
+              <tr key={user.id}>
                 <td>
-                  {editId === data._id ? (
+                  <Form.Check
+                    type="checkbox"
+                    checked={selectedUsers.includes(user.id)}
+                    onChange={() => handleSelectUser(user.id)}
+                  />
+                </td>
+                <td>{indexOfFirstUser + index + 1}</td>
+                <td>{user.id}</td>
+                <td>
+                  {editId === user.id ? (
                     <Form.Control
                       type="text"
-                      value={editData}
-                      onChange={(e) => setEditData(e.target.value)}
+                      value={editName}
+                      onChange={(e) => setEditName(e.target.value)}
                       style={{ backgroundColor: "lightblue" }}
                     />
                   ) : (
-                    data.data
+                    user.name
                   )}
                 </td>
                 <td>
-                  {editId === data._id ? (
+                  {editId === user.id ? (
+                    <Form.Control
+                      type="email"
+                      value={editEmail}
+                      onChange={(e) => setEditEmail(e.target.value)}
+                      style={{ backgroundColor: "lightblue" }}
+                    />
+                  ) : (
+                    user.email
+                  )}
+                </td>
+                <td>
+                  {editId === user.id ? (
+                    <Form.Control
+                      type="tel"
+                      value={editPhone}
+                      onChange={(e) => setEditPhone(e.target.value)}
+                      style={{ backgroundColor: "lightblue" }}
+                    />
+                  ) : (
+                    user.phone
+                  )}
+                </td>
+                <td>
+                  {editId === user.id ? (
                     <>
                       <Button
                         variant="success"
-                        onClick={() => handleEditData(data._id)}
+                        onClick={() => handleSave(user.id)}
                       >
                         Save
                       </Button>{" "}
@@ -115,16 +234,13 @@ export const Home = () => {
                     <>
                       <Button
                         variant="warning"
-                        onClick={() => {
-                          setEditId(data._id);
-                          setEditData(data.data);
-                        }}
+                        onClick={() => handleEdit(user)}
                       >
                         Edit
                       </Button>{" "}
                       <Button
                         variant="danger"
-                        onClick={() => handleDeleteData(data._id)}
+                        onClick={() => handleDelete(user.id)}
                       >
                         Delete
                       </Button>
@@ -135,6 +251,17 @@ export const Home = () => {
             ))}
           </tbody>
         </Table>
+        <Pagination>
+          {[...Array(totalPages).keys()].map((number) => (
+            <Pagination.Item
+              key={number + 1}
+              active={number + 1 === currentPage}
+              onClick={() => paginate(number + 1)}
+            >
+              {number + 1}
+            </Pagination.Item>
+          ))}
+        </Pagination>
       </Container>
       <Footer />
     </div>
